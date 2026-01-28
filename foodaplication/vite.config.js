ab16f04099e2474bc5b0f825d715c7d312037674
kaiff-ansari <mohdkaif8672@gmail.com>
2026-01-28 19:49:28 +0530

    modified config

--- foodaplication/vite.config.js
+++ foodaplication/vite.config.js
@@ -5,10 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
-  server:{
-    port:3000,
-    strictPort:true
+    port: 3000,
+    strictPort: true
 
   }
-});
+})
